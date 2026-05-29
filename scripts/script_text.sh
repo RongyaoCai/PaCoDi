c84efce98ec38e33759c07d75bafd90e06396473
Rongyao Cai <12532012@zju.edu.cn>
2026-05-29 16:41:46 +0800

    Delete scripts directory

--- scripts/script_text.sh
+++
@@ -1,3 +0,0 @@
-python main.py --name etth1_24 --config_file Config/text/etth1.yaml --gpu 0 --mode text --model_name pacodi_ddpm --train
-python main.py --name etth1_24 --config_file Config/text/etth1.yaml --gpu 0 --mode text --model_name pacodi_ddpm --milestone 10 --num_samples 5
-python evaluate_text.py
--- scripts/script_uncond.sh
+++
@@ -1,3 +0,0 @@
-python main.py --name etth_24 --config_file Config/uncond/etth.yaml --gpu 0 --mode uncond --model_name pacodi_sde --train
-python main.py --name etth_24 --config_file Config/uncond/etth.yaml --gpu 0 --mode uncond --model_name pacodi_sde --milestone 10
-python evaluate_uncond.py
